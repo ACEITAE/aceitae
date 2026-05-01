8df52d2c61b927f796a426add74f98f78c0dba44
Valdir Silva <silva2700@hotmail.com>
2026-05-01 00:03:07 -0300

    Update supabase_config.py

--- supabase_config.py
+++ supabase_config.py
@@ -1,7 +1,11 @@
 from supabase import create_client, Client
+import os
 
-SUPABASE_URL = "https://gkmzczraggmjeizszenh.supabase.co"
-SUPABASE_KEY = "sb_publishable_kmIUj96K0A_-qNOY5E4_9g_hVNssgix"
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("As variáveis SUPABASE_URL e SUPABASE_KEY precisam estar configuradas no ambiente")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
