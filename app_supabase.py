6d0a74e1cd734cfc7d2d0016a4240ecf417db64a
Valdir Silva <silva2700@hotmail.com>
2026-05-02 15:37:34 -0300

    Update app_supabase.py

--- app_supabase.py
+++ app_supabase.py
@@ -33,7 +33,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+@app.options("/{rest_of_path:path}")
+async def preflight_handler():
+    return Response(status_code=200)
 # ==================================================
 # SEGURANÇA
 # ==================================================
